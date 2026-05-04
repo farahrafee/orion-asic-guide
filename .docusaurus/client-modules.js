2a297476bb9228bcfcb79aef7db5d9c41a7e32b0
Farah Rafee <frafee@ners-farah.engin.umich.edu>
2026-05-04 10:27:43 -0400

    Deploy website - based on 9e05593c2156cd8348dfff2d256cfe048f59b142

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/frafee/backend-flow/node_modules/infima/dist/css/default/default.css"),
-  require("/home/frafee/backend-flow/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/frafee/backend-flow/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/frafee/backend-flow/src/css/custom.css"),
-];
